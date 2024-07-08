31cd5caf929de4f3cba23fff5dbf0d0478365502
xXmarcosX <marcos.v3567@gmail.com>
2024-07-07 23:30:02 -0300

    refatoração de códigos

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -24,10 +24,6 @@ const Banner = () => {
               height="415"
               src="https://www.youtube.com/embed/c8mVlakBESE?si=Bs4vOgLDRV-jdDOU"
               title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
             ></iframe>
           </div>
         </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import style from './Footer.module.css'
-import Logo from './img/LogoMain.svg'
+import React from "react";
+import { CiCirclePlus, CiHome } from "react-icons/ci";
+import { Link, useLocation } from "react-router-dom";
+import style from "./Footer.module.css";
+import FooterLink from "./FooterLink/FooterLink";
+import Logo from "./img/LogoMain.svg";
 
 const Footer = () => {
+  const local = useLocation();
+
   return (
     <>
-      <header className={style.footer}>
+      <footer className={style.footer}>
         <img src={Logo} alt="" />
-      </header>
+        <div className={style.footerBtns}>
+          {local.pathname === '/' ? (
+            <FooterLink rota={"/"}>
+              <CiHome size={30} /> home
+            </FooterLink>
+          ) : (
+            <Link to={"/"}>
+              <CiHome size={30} style={{color: 'white'}}/>
+            </Link>
+          )}
+          {local.pathname === '/novoVideo' ? (
+            <FooterLink rota={"/"}>
+              <CiCirclePlus  size={30}/> NOVO VIDEO
+            </FooterLink>
+          ) : (
+            <Link to={"/novoVideo"}>
+              <CiCirclePlus size={30} style={{color: 'white'}}/>
+            </Link>
+          )}
+        </div>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
